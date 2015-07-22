cc8484f7f4038e8e9e20722a9d14c3c1ff071fd8
Gaurav Singh <sgaurav.baghel@gmail.com>
2015-07-22 22:47:35 +0530

    status should be used from user patch endpoint

--- resources/users/index.js
+++ resources/users/index.js
@@ -1,16 +1,11 @@
 var conf = require('../../conf');
 var users = require('./users');
-var status = require('./status');
 
 // FIXME -- CHECK API AUTH HERE
 module.exports = function(app){
   app.get(conf.API_BASE + 'users', userInfo);
   app.patch(conf.API_BASE + 'users', userUpdate);
   app.delete(conf.API_BASE + 'users', userDelete);
-
-  // users/status
-  app.get(conf.API_BASE + 'users/status', statusInfo);
-  app.patch(conf.API_BASE + 'users/status', statusUpdate);
 };
 
 function userInfo(req, res, next){
@@ -40,34 +35,4 @@ function userUpdate(req, res, next){
 
 function userDelete(req, res, next){
   return res.send(200)
-};
-
-function statusInfo(req, res, next){
-  return status.fetch().then(function(result){
-    return res.status(200).send({
-      status: 'OK',
-      data: {
-        online: 'true'
-      }
-    });
-  })
-  .catch(function(err){
-    return res.status(500).send({
-      status: 'ERROR',
-      message: 'Something went wrong'
-    });
-  });
-};
-
-function statusUpdate(req, res, next){
-  var params = req.body.params;
-  return status.update(params).then(function(result){
-    return res.sendStatus(200);
-  })
-  .catch(function(err){
-    return res.status(500).send({
-      status: 'ERROR',
-      message: 'Something went wrong'
-    });
-  });
 };
