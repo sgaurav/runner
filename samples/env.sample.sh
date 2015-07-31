ae0355011653be8e0c738e3f754624a82b4c49b1
Gaurav Singh <sgaurav.baghel@gmail.com>
2015-07-31 09:47:02 +0530

    updated gitignore for env.sh

---
+++ samples/env.sample.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+export NODE_ENV=''
+export PORT=3000
+export SITE_SECRET='xxxxxxxxxxxxxxxx'
+export REDIS_HOST='127.0.0.1'
+export REDIS_PORT=6379
+export POSTGRES_USER='xxxxxxxxxxxxxxxx'
+export POSTGRES_DB='Runner'
+export POSTGRES_PORT=5432
+export POSTGRES_HOST='localhost'
+export POSTGRES_PASSWORD=null
