00e6b6bae4e5fcbe4e2da5c9822f4c4d2342ca88
Gaurav Singh <sgaurav.baghel@gmail.com>
2015-08-01 00:16:17 +0530

    moved lodash mixin to utils

--- utils/utils.js
+++ utils/utils.js
@@ -1,3 +1,5 @@
+var _ = require('lodash');
+
 function aliases(obj, ref){
   var temp = {};
   var keys = Object.keys(obj);
@@ -10,6 +12,18 @@ function aliases(obj, ref){
   });
   return temp;
 };
+
+// custom lodash function to check for falsies and remove them
+var removeFalsies = function (obj) {
+  return _.transform(obj, function (o, v, k) {
+    if (v && typeof v === 'object') {
+      o[k] = _.removeFalsies(v);
+    } else if (v !== null) {
+      o[k] = v;
+    }
+  });
+};
+_.mixin({ 'removeFalsies': removeFalsies });
 
 module.exports = {
   aliases: aliases
